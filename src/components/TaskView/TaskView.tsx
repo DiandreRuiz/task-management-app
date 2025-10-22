9ac5cce292a4d5f6b6e27fb23d279bbb5a22a262
DiandreRuiz <Diandreruiz77@gmail.com>
2025-10-22 11:06:16 -0400

    Added focused task highlighting and scaling to task view screen

--- src/components/TaskView/TaskView.tsx
+++ src/components/TaskView/TaskView.tsx
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import styles from "../../styles/TaskView.module.css";
 
 // All tasks mapped into slots that have dropdowns
 // Open tasks page we just see dropdowns
@@ -14,15 +15,15 @@ const TaskView: React.FC = () => {
     const { tasks } = useTasks();
     const params = useParams();
     const focusedTaskName = params.taskName;
-    console.log(focusedTaskName)
+    console.log(focusedTaskName);
 
     return (
         <Container className="mt-5">
             <Row>
                 {tasks.map((t) => (
                     <Col key={t.name} xs={12} md={4}>
-                        <Card className="mb-3">
-                            {focusedTaskName && t.name === focusedTaskName ? <Card.Header style={{backgroundColor: "gold"}}>{t.name}</Card.Header> : <Card.Header>{t.name}</Card.Header>}
+                        <Card className={focusedTaskName && t.name === focusedTaskName ? styles.focusedAnimation : ""}>
+                            <Card.Header>{t.name}</Card.Header>
                             <Card.Body>{t.description}</Card.Body>
                             <Card.Footer style={{ backgroundColor: t.taskColor ? t.taskColor : undefined }}>
                                 Group: <b>{t.taskGroup}</b>
