324038144c6ae66da3d6fa4b7c461bfc82cfcf8d
DiandreRuiz <Diandreruiz77@gmail.com>
2025-10-22 14:50:52 -0400

    Added conditional rendering to NavBar options Dashboard and Tasks

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,10 +4,12 @@ import Navbar from "react-bootstrap/Navbar";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginLogoutButtons from "./LoginLogoutButtons";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Login / Logout will be managed by Auth0 context
 
 function AppNavbar() {
+    const { isAuthenticated } = useAuth0();
     const [navExpanded, setNavExpanded] = useState(false);
     return (
         <Navbar bg="dark" variant="dark" expand="lg" fixed="top" expanded={navExpanded} onToggle={() => setNavExpanded(!navExpanded)}>
@@ -16,12 +18,19 @@ function AppNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/dashboard" onClick={() => setNavExpanded(false)}>
-                            Dashboard
+                        <Nav.Link as={Link} to="/" onClick={() => setNavExpanded(false)}>
+                            Home
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/tasks" onClick={() => setNavExpanded(false)}>
-                            Tasks
-                        </Nav.Link>
+                        {isAuthenticated && (
+                            <>
+                                <Nav.Link as={Link} to="/dashboard" onClick={() => setNavExpanded(false)}>
+                                    Dashboard
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/tasks" onClick={() => setNavExpanded(false)}>
+                                    Tasks
+                                </Nav.Link>
+                            </>
+                        )}
                         <LoginLogoutButtons />
                     </Nav>
                 </Navbar.Collapse>
